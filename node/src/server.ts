026114d0832a91135a9d4e4348af698c1d7f75a4
SalehBal <94306843+SalehBal@users.noreply.github.com>
2023-03-23 13:10:03 +0400

    back to import

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,17 +1,17 @@
-const mongoose = require('dotenv');
-const dotenv = require('dotenv');
-const app = require('./app');
+import mongoose from 'dotenv';
+import dotenv from 'dotenv';
+import app from './app';
 
 dotenv.config({ path: './config.env' });
 console.log(process.env.DBUSER);
 const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@cluster0.lpj0afg.mongodb.net/?retryWrites=true&w=majority`;
 const port = 2002;
-mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true } as any)
-  .then(() => {
-    app.listen(port, () => {
-      console.log('Server listening the port 127.0.0.1:' + port + '/');
-    });
-    console.log('Connected to database !');
-  })
-  .catch(() => console.log('Something went wrong !'));
+// mongoose
+//   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true } as any)
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log('Server listening the port 127.0.0.1:' + port + '/');
+//     });
+//     console.log('Connected to database !');
+//   })
+//   .catch(() => console.log('Something went wrong !'));
