aa41787c451a208574185f8a6164fd54b3f39a37
SalehBal <94306843+SalehBal@users.noreply.github.com>
2023-03-25 18:07:59 +0400

    Auth form dome

--- react/src/routes/Auth.tsx
+++ react/src/routes/Auth.tsx
@@ -1,70 +1,23 @@
-import { Box, Button, Paper, Stack, TextField } from '@mui/material';
-import React, { useState } from 'react';
-import bgImg from '../assets/background.jpg';
-import { useForm, SubmitHandler } from 'react-hook-form';
-interface FormData {
-  email: string;
-  password: string;
-  username?: string;
-  passwordconfirm?: string;
-}
+import { Box, Button, Paper, Stack, Grid, TextField, FormControlLabel, Checkbox } from '@mui/material';
+import bgImg from '../assets/background2.jpg';
+import AuthForm from './forms/AuthForm';
+
+const backgroudImgStyles = { backgroundImage: `url(${bgImg})`, backgroundSize: 'cover', filter: 'blur(8px)', height: '100vh', width: '100vw', position: 'fixed', zIndex: '-1' };
+
 function Auth() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<FormData>();
-  const [isSignup, setIsSinup] = useState(false);
-  const pageStyles = { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' };
-
-  function changeToSignUp() {
-    setIsSinup(true);
-  }
-  function changeToLogIn() {
-    setIsSinup(false);
-  }
-
-  const onSubmitFn: SubmitHandler<FormData> = (data) => {
-    console.log(data);
-  };
-
   return (
-    <Box sx={pageStyles}>
-      <Box sx={{ backgroundImage: `url(${bgImg})`, backgroundSize: 'cover', filter: 'blur(8px)', height: '100vh', width: '100vw', position: 'fixed', zIndex: '-1' }}></Box>
-      <Paper sx={{ width: '50%', boxShadow: '5px 5px 14px 0px rgba(0,0,0,0.59)', padding: '24px', borderRadius: '10px' }}>
-        <form onSubmit={handleSubmit(onSubmitFn)}>
-          <Stack spacing={2}>
-            {isSignup ? (
-              <TextField label={errors.username ? 'Username is required' : 'Username'} variant='outlined' {...register('username', { required: isSignup })} error={errors.username ? true : false} />
-            ) : null}
-            <TextField label={errors.email ? 'Email is required' : 'Email'} variant='outlined' {...register('email', { required: true })} error={errors.email ? true : false} />
-            <TextField label={errors.password ? 'Password is required' : 'Password'} variant='outlined' {...register('password', { required: true })} error={errors.password ? true : false} />
-            {isSignup ? (
-              <TextField
-                label={errors.passwordconfirm ? 'Password confirm is required' : 'Password confirm'}
-                variant='outlined'
-                {...register('passwordconfirm', { required: isSignup })}
-                error={errors.passwordconfirm ? true : false}
-              />
-            ) : null}
-            <Button type='submit' variant='contained'>
-              Submit
-            </Button>
-            {!isSignup ? (
-              <Button onClick={changeToSignUp} variant='text'>
-                Don't have an account? Signup here
-              </Button>
-            ) : null}
-            {isSignup ? (
-              <Button onClick={changeToLogIn} variant='text'>
-                Have an account? Login here
-              </Button>
-            ) : null}
-          </Stack>
-        </form>
-      </Paper>
-    </Box>
+    <>
+      <Box sx={backgroudImgStyles}></Box>
+      <Grid>
+        <Grid item xs={12}>
+          <Grid item xs={12} container justifyContent='center' alignItems='center' sx={{ minHeight: { xs: 'calc(100vh - 134px)', md: 'calc(100vh - 112px)' } }}>
+            <Grid item xl={8} lg={10} md={10} sm={11} xs={11.5}>
+              <AuthForm />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
---
+++ react/src/routes/forms/AuthForm.tsx
@@ -0,0 +1,93 @@
+import { Box, Button, Paper, Stack, Grid, TextField, FormControlLabel, Checkbox } from '@mui/material';
+import React, { useState } from 'react';
+import bgImg from '../assets/background.jpg';
+import { useForm, SubmitHandler } from 'react-hook-form';
+interface FormData {
+  email: string;
+  password: string;
+  username?: string;
+  passwordconfirm?: string;
+}
+function AuthForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
+  const [isSignup, setIsSinup] = useState(false);
+  function changeToSignUp() {
+    setIsSinup(true);
+  }
+  function changeToLogIn() {
+    setIsSinup(false);
+  }
+
+  const onSubmitFn: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+  return (
+    <Paper sx={{ boxShadow: '5px 5px 14px 0px rgba(0,0,0,0.59)', padding: '24px', borderRadius: '10px' }}>
+      <form onSubmit={handleSubmit(onSubmitFn)}>
+        <Grid container spacing={2}>
+          <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+            {isSignup ? (
+              <TextField
+                fullWidth={true}
+                label={errors.username ? 'Username is required' : 'Username'}
+                variant='outlined'
+                {...register('username', { required: isSignup })}
+                error={errors.username ? true : false}
+              />
+            ) : null}
+          </Grid>
+          <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+            <TextField fullWidth={true} label={errors.email ? 'Email is required' : 'Email'} variant='outlined' {...register('email', { required: true })} error={errors.email ? true : false} />
+          </Grid>
+          <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+            <TextField
+              fullWidth={true}
+              label={errors.password ? 'Password is required' : 'Password'}
+              variant='outlined'
+              {...register('password', { required: true })}
+              error={errors.password ? true : false}
+            />
+          </Grid>
+          <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+            {isSignup ? (
+              <TextField
+                fullWidth={true}
+                label={errors.passwordconfirm ? 'Password confirm is required' : 'Password confirm'}
+                variant='outlined'
+                {...register('passwordconfirm', { required: isSignup })}
+                error={errors.passwordconfirm ? true : false}
+              />
+            ) : null}
+          </Grid>
+          <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+            <Button fullWidth={true} type='submit' variant='contained'>
+              Submit
+            </Button>
+          </Grid>
+          <Grid item xl={6} lg={6} md={6} sm={12} xs={12}>
+            {!isSignup ? (
+              <Button fullWidth={true} onClick={changeToSignUp} variant='text'>
+                Don't have an account? Signup here
+              </Button>
+            ) : null}
+            {isSignup ? (
+              <Button fullWidth={true} onClick={changeToLogIn} variant='text'>
+                Have an account? Login here
+              </Button>
+            ) : null}
+          </Grid>
+          <Grid item xl={6} lg={6} md={6} sm={12} xs={12}>
+            <FormControlLabel control={<Checkbox />} label='Keep me logged in' />
+          </Grid>
+        </Grid>
+      </form>
+    </Paper>
+  );
+}
+
+export default AuthForm;
